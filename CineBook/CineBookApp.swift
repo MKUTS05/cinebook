ed56cb1033995c419cc020f25b2640a90b197ff7
Muhammad Khabbaz <muhammad.n.khabbaz@student.uts.edu.au>
2026-05-05 15:16:01 +1000

    Initial Commit

---
+++ CineBook/CineBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineBookApp.swift
+//  CineBook
+//
+//  Created by MK UNI on 5/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct CineBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
