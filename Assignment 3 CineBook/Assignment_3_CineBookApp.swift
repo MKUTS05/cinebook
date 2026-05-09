1c63c3461a5def9183a4cd42eade250d4a9004dc
Jovan Sutardi <jovan.sutardi@student.uts.edu.au>
2026-05-09 22:26:57 +1000

    Initial Commit

---
+++ Assignment 3 CineBook/Assignment_3_CineBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_3_CineBookApp.swift
+//  Assignment 3 CineBook
+//
+//  Created by Jovan Sutardi on 9/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_3_CineBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
